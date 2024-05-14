f3b11e505da2a9ea1657229adba0ffd2c4c6e355
vagrant <edarieta09@gmail.com>
2024-05-14 17:51:19 +0000

    displays 3 photos

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -4,18 +4,25 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   const { username, imageSource, id, location, profile } = props;
 
+  const photos = new Array(3).fill(null);
+
   return (
-    <div className="photo-list__item" id={`photo-${id}`}>
-      <img src={imageSource} alt={`Photo by ${username} taken in ${location.city}, ${location.country}`} className="photo-list__image" />
-      <div className="photo-list__user-details">
-        <img src={profile} alt={`${username}'s profile`}className="photo-list__user-profile" />
-        <div className="photo-list__user-info">
-          <h2>{username}</h2>
-          <h2 className="photo-list__user-location">{location.city}, {location.country}</h2>
+    <>
+      {photos.map((_, index) => (
+        <div key={index} className="photo-list__item" id={`photo-${id}-${index}`}>
+          <img src={imageSource} alt={`Photo by ${username} taken in ${location.city}, ${location.country}`} className="photo-list__image" />
+          <div className="photo-list__user-details">
+            <img src={profile} alt={`${username}'s profile`} className="photo-list__user-profile" />
+            <div className="photo-list__user-info">
+              <h2>{username}</h2>
+              <h2 className="photo-list__user-location">{location.city}, {location.country}</h2>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
 
 export default PhotoListItem;
+
