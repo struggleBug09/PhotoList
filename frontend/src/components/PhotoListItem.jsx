ab210dcc2cc427c805149bd5ba145b03cf3eb7ac
vagrant <edarieta09@gmail.com>
2024-05-22 20:28:06 +0000

    bug fix

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,17 +1,39 @@
+// import React from "react";
+// import PhotoFavButton from './PhotoFavButton';
+// import "../styles/PhotoListItem.scss";
+
+// const PhotoListItem = ({ photo, isFavorited, toggleFavorite, onPhotoClick }) => {
+//   return (
+//     <div className="photo-list__item" id={`photo-${photo.id}`} onClick={onPhotoClick}>
+//       <PhotoFavButton isFavorited={isFavorited} toggleFavorite={toggleFavorite} />
+//       <img src={photo.urls.regular} alt={`Photo by ${photo.user.username}`} className="photo-list__image" />
+//       <div className="photo-list__user-details">
+//         <img src={photo.user.profile} alt={`${photo.user.username}'s profile`} className="photo-list__user-profile" />
+//         <div className="photo-list__user-info">
+//           <h2>{photo.user.username}</h2>
+//           <h2 className="photo-list__user-location">{photo.location.city}, {photo.location.country}</h2>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default PhotoListItem;
+
 import React from "react";
 import PhotoFavButton from './PhotoFavButton';
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = ({ photo, isFavorited, toggleFavorite, onPhotoClick }) => {
   return (
-    <div className="photo-list__item" id={`photo-${photo.id}`} onClick={onPhotoClick}>
+    <div className="photo-list__item" onClick={() => onPhotoClick(photo)}>
       <PhotoFavButton isFavorited={isFavorited} toggleFavorite={toggleFavorite} />
       <img src={photo.urls.regular} alt={`Photo by ${photo.user.username}`} className="photo-list__image" />
       <div className="photo-list__user-details">
         <img src={photo.user.profile} alt={`${photo.user.username}'s profile`} className="photo-list__user-profile" />
         <div className="photo-list__user-info">
           <h2>{photo.user.username}</h2>
-          <h2 className="photo-list__user-location">{photo.location.city}, {photo.location.country}</h2>
+          <p>{photo.location.city}, {photo.location.country}</p>
         </div>
       </div>
     </div>
@@ -19,7 +41,4 @@ const PhotoListItem = ({ photo, isFavorited, toggleFavorite, onPhotoClick }) => 
 };
 
 export default PhotoListItem;
-
-
-
 
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,14 +1,46 @@
-import React from 'react';
+// import React from 'react';
 
+// import '../styles/PhotoDetailsModal.scss';
+// import closeSymbol from '../assets/closeSymbol.svg';
+
+// const PhotoDetailsModal = ({ toggleModal}) => {
+//   return (
+//     <div className="photo-details-modal">
+//       <button className="photo-details-modal__close-button" onClick={toggleModal}>
+//         <img src={closeSymbol} alt="close symbol" />
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default PhotoDetailsModal;
+
+import React from 'react';
 import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = ({ toggleModal}) => {
+const PhotoDetailsModal = ({ toggleModal, selectedPhoto }) => {
   return (
     <div className="photo-details-modal">
       <button className="photo-details-modal__close-button" onClick={toggleModal}>
-        <img src={closeSymbol} alt="close symbol" />
+        <img src={closeSymbol} alt="Close" />
       </button>
+      {selectedPhoto && (
+        <div>
+          <img
+            src={selectedPhoto.urls.regular}
+            alt={`Photo by ${selectedPhoto.user.username}`}
+            className="photo-details-modal__image"
+          />
+          <div className="photo-details-modal__photographer-details">
+            <img src={selectedPhoto.user.profile} alt={`${selectedPhoto.user.username}'s profile`} className="photo-details-modal__photographer-profile" />
+            <div className="photo-details-modal__photographer-info">
+              <h2>{selectedPhoto.user.username}</h2>
+              <p className="photo-details-modal__photographer-location">{selectedPhoto.location.city}, {selectedPhoto.location.country}</p>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
