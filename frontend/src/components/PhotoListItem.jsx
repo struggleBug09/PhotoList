44b687d2dea8c24f410d692bc41192c232006b05
vagrant <edarieta09@gmail.com>
2024-05-23 00:37:13 +0000

    removes unsused commented out code for readability

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -20,28 +20,3 @@ const PhotoListItem = ({ photo, isFavorited, toggleFavorite, onPhotoClick }) => 
 
 export default PhotoListItem;
 
-// import React from "react";
-// import PhotoFavButton from './PhotoFavButton';
-// import "../styles/PhotoListItem.scss";
-
-// const PhotoListItem = ({ photo, isFavorited, toggleFavorite, onPhotoClick }) => {
-//   return (
-//     <div className="photo-list__item" onClick={() => onPhotoClick(photo)}>
-//       <PhotoFavButton isFavorited={isFavorited} toggleFavorite={toggleFavorite} />
-//       <img src={photo.urls.regular} alt={`Photo by ${photo.user.username}`} className="photo-list__image" />
-//       <div className="photo-list__user-details">
-//         <span><img src={photo.user.profile} alt={`${photo.user.username}'s profile`} className="photo-list__user-profile" /></span>
-//         <div className="photo-list__user-info">
-//           {photo.user.username}
-//           <div className="photo-list__user-location">
-//             <span>{photo.location.city},</span>
-//             <span>{photo.location.country}</span>
-//           </div>
-//           {/* <p>{photo.location.city}, {photo.location.country}</p> */}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PhotoListItem;
--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,44 +1,3 @@
-// import React from 'react';
-// import '../styles/PhotoDetailsModal.scss';
-// import closeSymbol from '../assets/closeSymbol.svg';
-// import PhotoList from '../components/PhotoList';
-
-// const PhotoDetailsModal = ({ toggleModal, selectedPhoto }) => {
-//   if (!selectedPhoto) {
-//     console.error('No selected photo data available');
-//     return null;
-//   }
-
-//   const similarPhotosArray = selectedPhoto.similar_photos ? Object.values(selectedPhoto.similar_photos) : [];
-
-//   return (
-//     <div className="photo-details-modal">
-//       <button className="photo-details-modal__close-button" onClick={toggleModal}>
-//         <img src={closeSymbol} alt="Close" />
-//       </button>
-//       <img
-//         src={selectedPhoto.urls.regular}
-//         alt={`Photo by ${selectedPhoto.user.username}`}
-//         className="photo-details-modal__image"
-//       />
-//       <div className="photo-details-modal__photographer-details">
-//         {/* Might have to hard code photographer details if needed*/}
-//       </div>
-
-//       {similarPhotosArray.length > 0 && (
-//         <div className="photo-details-modal__images">
-//           <div className="photo-details-modal__header">
-//             Similar Photos
-//           </div>
-//           <PhotoList photos={similarPhotosArray} />
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default PhotoDetailsModal;
-
 import React from 'react';
 import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
@@ -50,7 +9,6 @@ const PhotoDetailsModal = ({ toggleModal, selectedPhoto }) => {
     return null;
   }
 
-  // Destructure necessary data safely
   const {
     urls: { regular: photoUrl } = {},
     user: {
