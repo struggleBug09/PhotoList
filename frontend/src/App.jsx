81d57394272ca706bddf3e71ae69cd0d9560efd2
vagrant <edarieta09@gmail.com>
2024-05-24 18:06:36 +0000

    fixes bug of modal opening when fave from app.jsx, removes uncessesarry comments

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,3 @@
-// import React, { useState } from 'react';
-// import HomeRoute from './routes/HomeRoute';
-// import './App.scss';
-
-// const App = () => {
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-
-//   const toggleModal = () => {
-//     setIsModalOpen(!isModalOpen);
-//   };
-
-//   return (
-//     <div className="App">
-//       <HomeRoute isModalOpen={isModalOpen} toggleModal={toggleModal} />
-//     </div>
-//   );
-// };
-
-// export default App;
-
 import React, { useState } from 'react';
 import HomeRoute from './routes/HomeRoute';
 import './App.scss';
@@ -53,4 +33,3 @@ const App = () => {
 };
 
 export default App;
-
--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,12 +1,33 @@
+// import React from 'react';
+// import FavIcon from './FavIcon';
+// import '../styles/PhotoFavButton.scss';
+
+// const PhotoFavButton = ({ isFavorited, toggleFavorite }) => {
+//   return (
+//     <div className="photo-list__fav-icon" onClick={toggleFavorite}>
+//       <div className="photo-list__fav-icon-svg">
+//         <FavIcon selected={isFavorited} /> {/* Displays the favorited state correctly */}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default PhotoFavButton;
+
 import React from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 const PhotoFavButton = ({ isFavorited, toggleFavorite }) => {
+  const handleFavClick = (e) => {
+    e.stopPropagation(); // This stops the click event from bubbling up to parent elements.
+    toggleFavorite();
+  };
+
   return (
-    <div className="photo-list__fav-icon" onClick={toggleFavorite}>
+    <div className="photo-list__fav-icon" onClick={handleFavClick}>
       <div className="photo-list__fav-icon-svg">
-        <FavIcon selected={isFavorited} /> {/* Displays the favorited state correctly */}
+        <FavIcon selected={isFavorited} />
       </div>
     </div>
   );
