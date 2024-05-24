167979fe94e767ee155a3b951e676c5ef702acdd
vagrant <edarieta09@gmail.com>
2024-05-24 18:19:13 +0000

    removes more comments

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,26 +1,10 @@
-// import React from 'react';
-// import FavIcon from './FavIcon';
-// import '../styles/PhotoFavButton.scss';
-
-// const PhotoFavButton = ({ isFavorited, toggleFavorite }) => {
-//   return (
-//     <div className="photo-list__fav-icon" onClick={toggleFavorite}>
-//       <div className="photo-list__fav-icon-svg">
-//         <FavIcon selected={isFavorited} /> {/* Displays the favorited state correctly */}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PhotoFavButton;
-
 import React from 'react';
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 const PhotoFavButton = ({ isFavorited, toggleFavorite }) => {
   const handleFavClick = (e) => {
-    e.stopPropagation(); // This stops the click event from bubbling up to parent elements.
+    e.stopPropagation(); // Prevents modal from opening when clicking fave on homescreen
     toggleFavorite();
   };
 
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,25 +1,3 @@
-// import React from "react";
-// import PhotoFavButton from './PhotoFavButton';
-// import "../styles/PhotoListItem.scss";
-
-// const PhotoListItem = ({ photo, isFavorited, toggleFavorite, onPhotoClick }) => {
-//   return (
-//     <div className="photo-list__item" onClick={() => onPhotoClick(photo)}>
-//       <PhotoFavButton isFavorited={isFavorited} toggleFavorite={toggleFavorite} />
-//       <img src={photo.urls.regular} alt={`Photo by ${photo.user.username}`} className="photo-list__image" />
-//       <div className="photo-list__user-details">
-//         <img src={photo.user.profile} alt={`${photo.user.username}'s profile`} className="photo-list__user-profile" />
-//         <div className="photo-list__user-info">
-//           {photo.user.username}
-//           <div className="photo-list__user-location">{photo.location.city}, {photo.location.country}</div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PhotoListItem;
-
 import React from "react";
 import PhotoFavButton from './PhotoFavButton';
 import "../styles/PhotoListItem.scss";
